f74e3caf40bff8117ba7bfc56c0a68c402e9ffaa
Alexandre Baaklini <abaaklini@gmail.com>
2025-06-17 18:27:44 -0300

    fix: update module type to commonjs and adjust import paths for consistency

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+module.exports = {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
   detectOpenHandles: true,
